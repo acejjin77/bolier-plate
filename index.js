c735ef9bef57b7a390783261ae3a86dc835705ab
acejjin77 <acejjin7@gmail.com>
2022-03-07 13:30:56 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const port = 5000;
+
+const mongoose = require("mongoose");
+mongoose
+  .connect(
+    "mongodb+srv://acejjin7:park1124@bolier-plate.asxoa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+    }
+  )
+  .then(() => console.log("Mongo DB connected..."))
+  .catch((err) => console.log(err));
+
+app.get("/", (req, res) => res.send("Hello world"));
+
+app.listen(port, () => console.log(`app port: ${port}`));
