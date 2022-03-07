764cde04de74331714dcf63ae11aec8ab2d299f1
acejjin77 <acejjin7@gmail.com>
2022-03-08 00:42:02 +0900

    mongoDB security issue

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const port = 5000;
 
+const config = require("./config/key");
+
+const { User } = require("./models/User");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
+
 const mongoose = require("mongoose");
 mongoose
-  .connect(
-    "mongodb+srv://acejjin7:park1124@bolier-plate.asxoa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-    }
-  )
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+  })
   .then(() => console.log("Mongo DB connected..."))
   .catch((err) => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello world21312321312"));
+app.get("/", (req, res) => res.send("Hello world!"));
+
+app.post("/register", (req, res) => {
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
+});
 
 app.listen(port, () => console.log(`app port: ${port}`));
